6fd3235c81806b4c1c244818c9127d73e448a236
AkbarAli <abrly@yahoo.com>
2024-05-15 20:35:26 +0400

    added buildspecs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,  
+  API_Token: 'mPS3Bvu494',
+  API_Prefix : 'https://z8878rixm9.execute-api.us-east-1.amazonaws.com/prod/'
 };
